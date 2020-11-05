911fcfe3105f811ecab95e20946165259732fab1
Javad Baghi <jaawaad.dev@gmail.com>
2020-11-06 03:09:40 +0330

    Hidden game panel and add some methods

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
+    startGame: false,
+  },
+  methods: {
+    runGame: function () {
+      this.startGame = true;
+    },
+    attack: {},
+    specialAttack: {},
+    heal: {},
+    giveUp: {},
   },
 });
